dbc2ce7423880645868f472454704b5328fa8173
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-07-17 01:10:14 +0700

    fix(apps/server): auth logic error

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -7,7 +7,7 @@ export class AuthService {
     constructor(private readonly prisma: PrismaService) {}
 
     async isUser(token: string | undefined) {
-        if (token?.length ?? 0 < 50) return false;
+        if ((token?.length ?? 0) < 50) return false;
 
         return (await this.prisma.accessToken.count({ where: { token } })) == 1;
     }
