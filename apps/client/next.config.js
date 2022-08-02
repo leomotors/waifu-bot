d0a2254738521a396a9ab77db2f5f3ebfe17083c
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-08-02 22:27:08 +0700

    client: add @next/bundle-analyzer

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,15 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // @ts-check
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const withTM = require("next-transpile-modules")(["@waifu-bot/graphql"]);
 
 /** @type {import("next").NextConfig} */
-const nextConfig = withTM({
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["cdn.discordapp.com", "c.tenor.com"],
-  },
-});
+const nextConfig = withBundleAnalyzer(
+  withTM({
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ["cdn.discordapp.com", "c.tenor.com"],
+    },
+  })
+);
 
 module.exports = nextConfig;
