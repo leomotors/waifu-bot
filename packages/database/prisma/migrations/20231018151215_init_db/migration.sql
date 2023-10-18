1ce1341001c1ca7f9f45bd76e4e6a4c7e71b739f
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-10-18 22:20:31 +0700

    feat: init web and db

---
+++ packages/database/prisma/migrations/20231018151215_init_db/migration.sql
@@ -0,0 +1,66 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "ActivityType" AS ENUM ('Playing', 'Streaming', 'Listening', 'Watching', 'Competing');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatar_url" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "waifu" (
+    "id" TEXT NOT NULL,
+    "note" TEXT NOT NULL DEFAULT '',
+    "name_en" TEXT NOT NULL,
+    "name_ja" TEXT NOT NULL,
+    "short_name_en" TEXT NOT NULL,
+    "short_name_ja" TEXT NOT NULL,
+    "footer_text" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "banner_url" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "created_by_user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "waifu_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "simp_interval" (
+    "id" SERIAL NOT NULL,
+    "waifu_id" TEXT NOT NULL,
+    "begin" TIMESTAMP(3) NOT NULL,
+    "end" TIMESTAMP(3) NOT NULL,
+    "days" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "activity" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT,
+    "type" "ActivityType" NOT NULL,
+    "enabled" BOOLEAN NOT NULL DEFAULT true
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "simp_interval_id_key" ON "simp_interval"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "activity_id_key" ON "activity"("id");
+
+-- AddForeignKey
+ALTER TABLE "waifu" ADD CONSTRAINT "waifu_created_by_user_id_fkey" FOREIGN KEY ("created_by_user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "simp_interval" ADD CONSTRAINT "simp_interval_waifu_id_fkey" FOREIGN KEY ("waifu_id") REFERENCES "waifu"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
